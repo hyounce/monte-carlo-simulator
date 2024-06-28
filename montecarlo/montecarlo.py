62e4338054052450e6817635bc613de0f6b28e7d
hyounce <hilde.younce@gmail.com>
2024-06-28 11:00:19 -0400

    Added class methods

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,15 +1,41 @@
 class Die:
-    
+
     def __init__(self):
+        pass
+    
+    def set_weight(self, face_val, new_weight):
+        pass
+
+    def roll_die(self, nrolls):
+        pass
+
+    def current_state(self):
         pass
 
 class Game:
     
-    def __init__(self):
+    def __init__(self, dice):
         pass
 
+    def play(self, nrolls):
+        pass
+
+    def recent_results(self):
+        pass
 
 class Analyzer:
 
-    def __init__(self):
+    def __init__(self, game):
+        pass
+
+    def jackpot(self):
+        pass
+
+    def face_counts_per_roll(self):
+        pass
+
+    def combo_count(self):
+        pass
+
+    def permutation_count(self):
         pass
