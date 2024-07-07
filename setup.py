77a30ab5af500d5fb188231855908c94343f1d11
hyounce <hilde.younce@gmail.com>
2024-07-07 17:51:25 -0400

    Added setup details

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ds5100-finalproject-ksg8xy',
+    version='1.0.0',
+    url='https://github.com/hyounce/ds5100-finalproject-ksg8xy',
+    author='hyounce',
+    author_email='ksg8xy@virginia.edu',
+    description="Contains montecarlo package for DS 5100's final project.",
+    packages=['montecarlo'],
+)
