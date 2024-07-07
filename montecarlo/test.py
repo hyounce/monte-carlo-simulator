43827c7ad2b549a9a6da232c84f275358cb8c400
hyounce <hilde.younce@gmail.com>
2024-07-07 17:54:06 -0400

    Renamed file

--- montecarlo/test.py
+++
@@ -1,9 +0,0 @@
-from montecarlo import Die
-import numpy as np
-
-arr = np.array(['q', 'g', 'k', 'l'])
-d = Die(arr)
-print(d.weights)
-d.set_weight('k', 5.0)
-print(d.current_state())
-print(d.roll_die(8))
