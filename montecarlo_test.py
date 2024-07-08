f4f8b9599f868aefb6c5025d8a66511a712e5444
hyounce <hilde.younce@gmail.com>
2024-07-07 20:23:11 -0400

    Modified formatting

--- montecarlo_test.py
+++ montecarlo_test.py
@@ -1,7 +1,11 @@
-from montecarlo import montecarlo
+from montecarlo.montecarlo import Die
 import unittest
 
 class MontecarloTestSuite(unittest.TestCase):
     
     def test_die(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
