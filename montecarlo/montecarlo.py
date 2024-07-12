0fd4a22b7b0e3fae47b28d74323920728c2dde78
hyounce <hilde.younce@gmail.com>
2024-07-12 12:34:27 -0400

    Add: Docstrings

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -3,8 +3,31 @@ import pandas as pd
 import random
 
 class Die:
+    '''
+    A class representing a die object with N faces and W weights, 
+    which can be rolled to select a face one or more times.  
+
+    Attributes: 
+    - faces: A Numpy Array of unique symbols. Accepts numeric or string values.
+    - weights: A list of weights corresponding to each face. Defaults to 1.0 for each face.
+    - die_df: A pandas dataframe with faces as the index and a column for their corresponding weights. 
+
+    Methods:
+    - set_weight(face, int): Changes a face's weight to a new value.
+    - roll_die(n_rolls=1): Selects a face n_rolls times. 
+    '''
 
     def __init__(self, faces):
+        '''
+        Initializes the Die with faces and default weights of 1.0. 
+
+        Input:
+        - faces: A Numpy Array of unique numeric or string values. 
+
+        Raises:
+        - TypeError: If faces not a Numpy Array.
+        - ValueError: If values in array are not numerics or strings, or if there are repeating values.
+        '''
 
         types = ['i', 'f', 'U']
 
@@ -22,6 +45,17 @@ class Die:
 
     
     def set_weight(self, face_val, new_weight):
+        '''
+        Changes a face's weight to a new value.
+
+        Input:
+        - face_val: String or numeric value corresponding to a value in faces.
+        - new_weight: int or float
+
+        Raises:
+        - IndexError: If face_val not in faces array.
+        - TypeError: If new_weight not int or float.
+        '''
 
         if face_val not in self.faces:
             raise IndexError("Value not in faces array.")
@@ -32,19 +66,56 @@ class Die:
             self.weights = self.die_df['weights'].tolist()
 
     def roll_die(self, n_rolls=1):
+        '''
+        Selects a face one or more times and returns a list of results.
+
+        Input:
+        - n_rolls: int representing how many times to roll. Defaults to 1.
+
+        Output:
+        - rolls: Returns list of faces that were selected from each roll.
+        '''
 
         rolls = random.choices(self.faces, weights=self.weights, k=n_rolls)
         return rolls
 
     def current_state(self):
+        '''
+        Returns a dataframe storing the die's faces and their corresponding weights.
+
+        Output:
+        - die_df: A Pandas dataframe. 
+        '''
 
         return self.die_df
 
 class Game:
+    '''
+    A class representing a game object consisting of one or more similar dice (Die objects) 
+    one or more times. 
 
+    Attributes:
+    - __play_df: A private Pandas dataframe that stores roll numbers in an index and 
+        columns with the results for each die.
+    - dice: A list of one or more similar Die objects.
+
+    Methods:
+    - play(nrolls): Rolls the dice nrolls times and stores the results in the private dataframe.
+    - recent_results(wide=True): Returns the private dataframe of game results in a wide or 
+        narrow format. Defaults to wide=True.
+    '''
     __play_df = pd.DataFrame()
 
     def __init__(self, dice):
+        '''
+        Initializes the Game with a list of Die objects.
+
+        Inputs: 
+        - dice: A list of one or more similar Die objects.
+
+        Raises:
+        - TypeError: If values of dice are not Die objects.
+        '''
 
         for die in dice:
             if not isinstance(die, Die):
@@ -53,8 +124,12 @@ class Game:
         self.dice = dice
 
     def play(self, nrolls):
+        '''
+        Rolls all the dice a specified number of times and stores the results in a dataframe.
 
-        # self.play_df = pd.DataFrame()  
+        Inputs:
+        - nrolls: int representing the number of times to roll the dice.
+        '''
 
         for die in self.dice:
             play = die.roll_die(nrolls)
@@ -63,6 +138,15 @@ class Game:
         self.__play_df.index.name = 'roll number'
 
     def recent_results(self, wide=True):
+        '''
+        Returns the dataframe of results of the most recent play, either in wide or narrow format.
+
+        Inputs:
+        - wide: Boolean value corresponding to wide or narrow. Defaults to True.
+
+        Raises:
+        - ValueError: If wide not set to True or False. 
+        '''
 
         if wide == True:
             return self.__play_df
@@ -74,8 +158,30 @@ class Game:
             raise ValueError("Parameter wide accepts True or False.") 
 
 class Analyzer:
+    '''
+    A class representing an Analyzer object that computes various statistics on the 
+    results of a Game object.
+
+    Attributes: 
+    - game: A Game object. 
+
+    Methods:
+    - jackpot(): Returns the number of times all dice rolled the same face in a game. 
+    - face_counts_per_roll(): Computes the number of times each die face was rolled in a game.
+    - combo_count(): Computes the distinct combinations of faces rolled along with their counts.
+    - permutation_count(): Computes the distinct permutations of faces rolled along with their counts.
+    '''
 
     def __init__(self, game):
+        '''
+        Initializes an Analyzer object with a Game object.
+
+        Inputs:
+        - game: A Game object. 
+
+        Raises:
+        - ValueError: If game is not a Game object.
+        '''
 
         if not isinstance(game, Game):
             raise ValueError("Parameter game must be Game object.")
@@ -83,12 +189,27 @@ class Analyzer:
             self.game = game
 
     def jackpot(self):
+        '''
+        Returns the number of times all dice rolled the same face in a game. 
+
+        Output:
+        - count: An int representing the number of jackpots in a single game. 
+        '''
 
         results = self.game.recent_results()
         jackpot = results.nunique(axis=1) == 1
-        return int(jackpot.sum())
+        count = int(jackpot.sum())
+        return count
 
     def face_counts_per_roll(self):
+        '''
+        Computes the number of times each die face was rolled in a game and returns a Pandas dataframe
+        of the results. 
+
+        Output:
+        - face_counts_df: A Pandas dataframe with an index of the roll number, face values as columns
+            and count values in the cells. 
+        '''
 
         results = self.game.recent_results()
         faces = self.game.dice[0].faces.tolist()
@@ -102,6 +223,13 @@ class Analyzer:
         return face_counts_df
 
     def combo_count(self):
+        '''
+        Computes the distinct combinations of faces rolled along with their counts and returns a 
+        Pandas dataframe of the results. 
+
+        Output:
+        - combo_df: A Pandas dataframe with a MultiIndex of combinations and a column for the associated counts.
+        '''
 
         results = self.game.recent_results()
         sorted_face_counts = results.apply(lambda row: sorted(row), axis=1)
@@ -111,6 +239,13 @@ class Analyzer:
         return combo_df
 
     def permutation_count(self):
+        '''
+        Computes the distinct permutations of faces rolled along with their counts and returns a Pandas
+        dataframe of the results. 
+
+        Output: 
+        - perms: A Pandas dataframe with a MultiIndex of permutations and a column for the associated counts. 
+        '''
 
         results = self.game.recent_results()
         perms = results.groupby(list(results.columns)).size()
